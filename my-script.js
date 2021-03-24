07ab08c39d769a9e31ca4424b705bbd2172018ee
yadonx <ej_yo@hotmail.com>
2021-03-24 12:12:33 +0100

    test

--- my-script.js
+++ my-script.js
@@ -6,6 +6,7 @@ if ($cart == null)
 updateCartIcon()
 
 function indexTable() {
+  // test
   $("#shopCartText").text($cartCounter)
   const $products = $("#products")
   fetch('https://webacademy.se/fakestore/')
